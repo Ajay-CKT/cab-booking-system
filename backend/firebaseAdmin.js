f38ab9b4b0e99771f35adac1bc7d384b6b5e52e3
Ajay T <122516656+Ajay-CKT@users.noreply.github.com>
2024-09-27 22:30:49 +0530

    Update firebaseAdmin.js

--- backend/firebaseAdmin.js
+++ backend/firebaseAdmin.js
@@ -1,6 +1,6 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("");
+const serviceAccount = require(""); // path to firebase-adminsdk.json
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
