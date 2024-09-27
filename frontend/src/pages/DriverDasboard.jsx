e52ed1d481683dd6f0d0094bb45872cedb1b26de
Ajay T <122516656+Ajay-CKT@users.noreply.github.com>
2024-09-27 22:24:08 +0530

    Update DriverDasboard.jsx

--- frontend/src/pages/DriverDasboard.jsx
+++ frontend/src/pages/DriverDasboard.jsx
@@ -21,7 +21,7 @@ const libraries = ["places"];
 
 const DriverDashboard = () => {
   const { isLoaded } = useJsApiLoader({
-    googleMapsApiKey: import.meta.env.VITE_googleMapAPI,
+    googleMapsApiKey: import.meta.env.VITE_googleMapAPI, // provide the api key
     libraries,
   });
   const navigate = useNavigate();
